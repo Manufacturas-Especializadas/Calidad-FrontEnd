3608efeb17b1112a8ae1890981c97a7916d511b1
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-09-26 09:47:54 -0600

    Ruta Admin

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,11 +2,14 @@ import { Route, Routes } from "react-router";
 import { RejectionIndex } from "../pages/Rejections/RejectionIndex";
 import { ProcessAudit } from "../pages/ProcessAudit/ProcessAudit";
 import { ProductAudit } from "../pages/ProductAudit/ProductAudit";
+import { AdminIndex } from "../pages/Admin/AdminIndex";
 
 export const MyRoutes = () => {
     return (
         <>
             <Routes>
+                {/* Admin */}
+                <Route path="/administrador" element={<AdminIndex />} />
 
                 {/* Rejections */}
                 <Route path="/" element={<RejectionIndex />} />
